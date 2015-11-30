8ff2c932113f32ec74ebf0c5ff701993c588ffa0
Peter Zhang <pzhang87@gmail.com>
2015-11-30 06:06:57 -0500

    set limit to 10, add load on scroll

--- js/giphy.js
+++ js/giphy.js
@@ -1,29 +1,35 @@
-// current status - takes the search value on click in the form box and appends
+offset = 0;
 
-// next step - use express and submit button to update the url as well as the search
+function getGIFs(){
+  var url = "http://api.giphy.com/v1/gifs/search?q=" + searchValue +
+    "&limit=10&offset=" + offset + "&api_key=dc6zaTOxFJmzC"
+  offset += 10;
 
-// next step - unlimited scroll works
-
-$(document).ready(function(){
-  $("#keyword").on("click", function(){
-    var searchValue = $("#keyword").val();
-    var url = "http://api.giphy.com/v1/gifs/search?q=" + searchValue +
-      "&api_key=dc6zaTOxFJmzC"
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(response){
-      $.each(response.data, function(index, image){
-        $("body").append('<embed src="'+this.embed_url+'">')
-        console.log(this)
-      })
-    }).fail(function(){
-      console.log("Ajax request fail!")
-    }).always(function(){
-      console.log("This always happens!")
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    $.each(response.data, function(index, image){
+      $("body").append('<embed src="'+this.embed_url+'">')
+      console.log(this)
     })
-
-
+  }).fail(function(){
+    console.log("Ajax request fail!")
+  }).always(function(response){
+    console.log(response)
   })
+}
+
+$("form").on("submit", function(event){
+  event.preventDefault();
+  offset = 0;
+  searchValue = $("#keyword").val();
+  getGIFs();
 })
+
+$(window).on("scroll", function() {
+  if ($(window).scrollTop() + $(window).height() >= $(document).height()){
+    getGIFs();
+  };
+});
